98729e8185050286518c79c428093c052df997e1
Ashishadhikari123 <adhikarigolu987@gmail.com>
2021-06-28 20:44:50 +0530

    binary search

---
+++ binary_search.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int>v{1,2,2,4,5,6,7};
+    int x=binary_search(v.begin(),v.end(),2);
+    if(x){
+        cout<<"Element found ";
+    }else{
+        cout<<"Element not found";
+    }
+    return 0;
+}
