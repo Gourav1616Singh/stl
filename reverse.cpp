d7fd75898415bad835431da0a713ec7af72b07c9
Ashishadhikari123 <adhikarigolu987@gmail.com>
2021-06-28 20:05:05 +0530

    reverse vector elements

---
+++ reverse.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void display(vector<int>&v)
+{
+    cout<<"Vector elements are"<<endl;
+    for(auto it=v.begin();it!=v.end();it++)
+    {
+        cout<<*it<<" ";
+    }
+}
+
+int main()
+{
+    int n,x;
+    cout<<"Enter vector size"<<endl;
+    cin>>n;
+    vector<int>v;
+    cout<<"Enter vector elements"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>x;
+        v.push_back(x);
+    }
+    reverse(v.begin(),v.end());
+    display(v);
+    return 0;
+}
